f6fedf61d1455c957956688ebea92105350216dc
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-14 21:32:15 +1200

    Init commit

---
+++ src/net/scriptronix/Main.java
@@ -0,0 +1,16 @@
+package net.scriptronix;
+
+/**
+ * @author Ben Mitchell (sid:23199999)
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // TODO code application logic here
+    }
+    
+}
