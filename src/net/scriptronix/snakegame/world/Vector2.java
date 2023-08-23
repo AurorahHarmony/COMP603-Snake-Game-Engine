0dc6bcbbb60e2586a1f3d43f4d76d10f3ef7419f
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-23 13:17:32 +1200

    Create basic Vector2 class

---
+++ src/net/scriptronix/snakegame/world/Vector2.java
@@ -0,0 +1,72 @@
+package net.scriptronix.snakegame.world;
+
+/**
+ * Structure for storing Vector2 data
+ */
+public class Vector2 {
+    private float x;
+    private float y;
+    
+    /**
+     * Creates a Vector2(x = 0, y = 0)
+     */
+    public Vector2() {
+        this(0, 0);
+    }
+    
+    /**
+     * Create a Vector2
+     * @param x
+     * @param y 
+     */
+    public Vector2(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return the x position
+     */
+    public float getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x position to set
+     */
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y position
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y position to set
+     */
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    /**
+     * Creates a new zero Vector2 (0,0)
+     * @return 
+     */
+    public static Vector2 zero() {
+        return new Vector2(0,0);
+    }
+    
+    /**
+     * Copy the values from another Vector2 into this Vector2
+     * @param vector2 The Vector2 to copy from
+     */
+    public void copyFrom(Vector2 vector2) {
+        this.x = vector2.x;
+        this.y = vector2.y;
+    }
+    
+}
