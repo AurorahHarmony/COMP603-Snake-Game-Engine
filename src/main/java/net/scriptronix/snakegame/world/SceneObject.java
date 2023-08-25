76d8a49bae1dc426d366a985dc527241e2dec2a0
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-25 13:18:32 +1200

    Add simple function to test input messaging

--- src/main/java/net/scriptronix/snakegame/world/SceneObject.java
+++ src/main/java/net/scriptronix/snakegame/world/SceneObject.java
@@ -1,19 +1,56 @@
 package net.scriptronix.snakegame.world;
 
+import net.scriptronix.snakegame.input.EInputAction;
 import net.scriptronix.snakegame.math.Vector2;
+import net.scriptronix.snakegame.message.IMessageHandler;
+import net.scriptronix.snakegame.message.Message;
 import net.scriptronix.snakegame.rendering.ConsolePixel;
 import net.scriptronix.snakegame.rendering.IConsoleRenderable;
 
 /**
  * Stores the information about an object in the scene.
  */
-public class SceneObject implements IConsoleRenderable{
+public class SceneObject implements IConsoleRenderable, IMessageHandler {
+
+    char symbol = 'x';
+
+    public SceneObject() {
+        this.init();
+    }
+
+    private void init() {
+        Message.subscribe("INPUT_ACTION", this);
+    }
 
     @Override
     public ConsolePixel[] getConsolePixels() {
-        ConsolePixel cPixel = new ConsolePixel(Vector2.zero(), 'x');
+        ConsolePixel cPixel = new ConsolePixel(Vector2.zero(), symbol);
         ConsolePixel[] cPixArr = {cPixel};
         return cPixArr;
     }
-    
+
+    @Override
+    public void onMessage(Message msg) {
+        if (msg.isCode("INPUT_ACTION")) {
+            switch ((EInputAction) msg.getContext()) {
+                case MOVE_LEFT:
+                    symbol = 'l';
+                    break;
+                case MOVE_UP:
+                    symbol = 'u';
+                    break;
+                case MOVE_RIGHT:
+                    symbol = 'r';
+                    break;
+                case MOVE_DOWN:
+                    symbol = 'd';
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+    }
+
 }
