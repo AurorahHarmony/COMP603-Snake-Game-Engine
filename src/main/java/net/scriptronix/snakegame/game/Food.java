6b95424a95ec7d049ba74514f61402aca547fb0e
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-26 16:46:15 +1200

    Randomly teleport food when it's eaten

--- src/main/java/net/scriptronix/snakegame/game/Food.java
+++ src/main/java/net/scriptronix/snakegame/game/Food.java
@@ -1,5 +1,6 @@
 package net.scriptronix.snakegame.game;
 
+import java.util.Random;
 import net.scriptronix.snakegame.math.Vector2;
 import net.scriptronix.snakegame.rendering.ConsolePixel;
 import net.scriptronix.snakegame.rendering.IConsoleRenderable;
@@ -11,14 +12,14 @@ import net.scriptronix.snakegame.world.SimpleCollisionEvent;
  * Represents a piece of food that the snake can eat
  */
 public class Food extends SceneObject implements IConsoleRenderable, ISimpleCollidable {
-    
-    char symbol = '@';
+
+    private final char symbol = '@';
+    private final Random rand = new Random();
 
     public Food() {
-        this.position = new Vector2(4,4);
+        this.position = new Vector2(4, 4);
     }
 
-    
     @Override
     public ConsolePixel[] getConsolePixels() {
         return new ConsolePixel[]{new ConsolePixel(position, symbol)};
@@ -26,9 +27,16 @@ public class Food extends SceneObject implements IConsoleRenderable, ISimpleColl
 
     @Override
     public void onCollision(SimpleCollisionEvent sce) {
-        if (sce.collidable() instanceof Snake) {
-            this.symbol = 'e';
-        }
+        if (sce.otherCollidable() instanceof Snake)
+            teleportToRandomLocation();
+    }
+    
+    /**
+     * Teleport the food to a new random location on screen
+     */
+    private void teleportToRandomLocation() {
+        this.position.setX(rand.nextInt(5));
+        this.position.setY(rand.nextInt(5));
     }
 
 }
