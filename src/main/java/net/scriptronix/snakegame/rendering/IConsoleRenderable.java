2a611423bf0e322a1b729e8c7f667204c1cdbfff
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-26 16:40:01 +1200

    Add comments

--- src/main/java/net/scriptronix/snakegame/rendering/IConsoleRenderable.java
+++ src/main/java/net/scriptronix/snakegame/rendering/IConsoleRenderable.java
@@ -4,5 +4,10 @@ package net.scriptronix.snakegame.rendering;
  * Interface defining a object which can be rendered by the console renderer.
  */
 public interface IConsoleRenderable {
+
+    /**
+     * @return An ConsolePixel[] specifying how this element should appear when
+     * rendered
+     */
     ConsolePixel[] getConsolePixels();
 }
--- src/main/java/net/scriptronix/snakegame/world/ISimpleCollidable.java
+++ src/main/java/net/scriptronix/snakegame/world/ISimpleCollidable.java
@@ -4,5 +4,9 @@ package net.scriptronix.snakegame.world;
  * Interface for objects that use simple collision (When they occupy the same space)
  */
 public interface ISimpleCollidable {
+    /**
+     * Triggered when another ISimpleColliable collides with this ISimpleCollidable
+     * @param sce 
+     */
     void onCollision(SimpleCollisionEvent sce);
 }
