05ce8cef437397940232c092ed2f2709067d0a1a
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-28 14:16:03 +1200

    Add equals check compatability

--- src/main/java/net/scriptronix/snakegame/math/Vector2.java
+++ src/main/java/net/scriptronix/snakegame/math/Vector2.java
@@ -4,20 +4,22 @@ package net.scriptronix.snakegame.math;
  * Structure for storing Vector2 data
  */
 public class Vector2 {
+
     private int x;
     private int y;
-    
+
     /**
      * Creates a Vector2(x = 0, y = 0)
      */
     public Vector2() {
         this(0, 0);
     }
-    
+
     /**
      * Create a Vector2
+     *
      * @param x
-     * @param y 
+     * @param y
      */
     public Vector2(int x, int y) {
         this.x = x;
@@ -51,26 +53,29 @@ public class Vector2 {
     public void setY(int y) {
         this.y = y;
     }
-    
+
     /**
      * Add the values from another Vector2 to this Vector2
-     * @param vec 
+     *
+     * @param vec
      */
     public void add(Vector2 vec) {
         this.x += vec.x;
         this.y += vec.y;
     }
-    
+
     /**
      * Creates a new zero Vector2 (0,0)
-     * @return 
+     *
+     * @return
      */
     public static Vector2 zero() {
-        return new Vector2(0,0);
+        return new Vector2(0, 0);
     }
-    
+
     /**
      * Copy the values from another Vector2 into this Vector2
+     *
      * @param vector2 The Vector2 to copy from
      */
     public void copyFrom(Vector2 vector2) {
@@ -82,9 +87,12 @@ public class Vector2 {
     public String toString() {
         return this.x + "," + this.y;
     }
-    
+
     public static Vector2 newFrom(Vector2 thatVector2) {
         return new Vector2(thatVector2.getX(), thatVector2.getY());
     }
-    
+
+    public boolean equals(Vector2 thatVector2) {
+        return (this.x == thatVector2.x) && (this.y == thatVector2.y);
+    }
 }
