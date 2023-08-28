3f9c09d8816a73761c7cf8176208a33f09c745ba
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-08-28 12:52:09 +1200

    Add bodyPart on food collision

--- src/main/java/net/scriptronix/snakegame/game/Snake.java
+++ src/main/java/net/scriptronix/snakegame/game/Snake.java
@@ -16,37 +16,49 @@ import net.scriptronix.snakegame.world.SimpleCollisionEvent;
  * The snake player class
  */
 public class Snake extends SceneObject implements IConsoleRenderable, IMessageHandler, ISimpleCollidable {
-
-    Vector2 velocity = new Vector2(0, -1);
-    ArrayList<Vector2> body; // Head is always the position. Body follows the head.
-    Integer size = 0;
-
+    
+    final private Vector2 velocity = new Vector2(0, -1);
+    final private ArrayList<Vector2> bodyParts = new ArrayList<>(); // Head is always the position. Body follows the head.
+    private Vector2 tailLastPosition;
+    
     public Snake(Scene scene) {
         super(scene);
         this.init();
     }
-
+    
     private void init() {
         int halfEngineWidth = getEngineConfig().getVirtualWidth() / 2;
         int halfEngineHeight = getEngineConfig().getVirtualHeight() / 2;
         this.position = new Vector2(halfEngineWidth, halfEngineHeight);
-
+        
         Message.subscribe("INPUT_ACTION", this);
-    }
 
+        // Add some starting parts to the snake
+        this.bodyParts.add(new Vector2(this.position.getX(), this.position.getY() - 1));
+        this.bodyParts.add(new Vector2(this.position.getX(), this.position.getY() - 2));
+        
+        this.tailLastPosition = Vector2.newFrom(this.bodyParts.get(this.bodyParts.size() - 1));
+    }
+    
     @Override
     public ConsolePixel[] getConsolePixels() {
-        ConsolePixel cPixel = new ConsolePixel(position, size.toString().charAt(0));
-        ConsolePixel[] cPixArr = {cPixel};
+        ConsolePixel[] cPixArr = new ConsolePixel[bodyParts.size() + 1];
+        
+        ConsolePixel cPixel = new ConsolePixel(position, 'O');
+        cPixArr[0] = cPixel;
+        for (int i = 0; i < bodyParts.size(); i++) {
+            cPixArr[i + 1] = new ConsolePixel(bodyParts.get(i), 'O');
+        }
+        
         return cPixArr;
     }
-
+    
     @Override
     public void onMessage(Message msg) {
         if (msg.isCode("INPUT_ACTION"))
             handleInput((EInputAction) msg.getContext());
     }
-
+    
     private void handleInput(EInputAction inputAction) {
         switch (inputAction) {
             case MOVE_LEFT:
@@ -69,18 +81,27 @@ public class Snake extends SceneObject implements IConsoleRenderable, IMessageHa
                 break;
         }
     }
-
+    
     @Override
     public void update() {
+        Vector2 lastPosition = Vector2.newFrom(this.position);
         position.add(velocity);
-
+        
         if (this.isOffScreen())
             System.exit(0);
+        
+        Vector2 currentPosition = Vector2.zero();
+        for (Vector2 bodyPart : this.bodyParts) {
+            currentPosition.copyFrom(bodyPart);
+            bodyPart.copyFrom(lastPosition);
+            lastPosition.copyFrom(currentPosition);
+        }
+        
     }
-
+    
     @Override
     public void onCollision(SimpleCollisionEvent sce) {
         if (sce.otherCollidable() instanceof Food)
-            size++;
+            bodyParts.add(Vector2.newFrom(this.tailLastPosition));
     }
 }
