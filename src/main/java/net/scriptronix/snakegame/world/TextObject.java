d7b0452f6fdec0da6b7edaa1c43ad99547b65d81
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-09-03 21:46:32 +1200

    Create renderable text

---
+++ src/main/java/net/scriptronix/snakegame/world/TextObject.java
@@ -0,0 +1,40 @@
+package net.scriptronix.snakegame.world;
+
+import java.util.ArrayList;
+import net.scriptronix.snakegame.math.Vector2;
+import net.scriptronix.snakegame.rendering.ConsolePixel;
+import net.scriptronix.snakegame.rendering.IConsoleRenderable;
+
+/**
+ * Displays a piece of text in the Scene
+ */
+public class TextObject extends SceneObject implements IConsoleRenderable {
+
+    private String displayText;
+
+    public TextObject(Scene scene, String text) {
+        super(scene);
+
+        this.displayText = text;
+    }
+
+    @Override
+    public ConsolePixel[] getConsolePixels() {
+        ArrayList<ConsolePixel> pixels = new ArrayList<>();
+        Vector2 pixelPosition;
+
+        char[] chars = this.displayText.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            pixelPosition = Vector2.newFrom(this.position);
+            pixelPosition.add(i, 0); // Offset one space right to allow space for the cursor
+            pixels.add(new ConsolePixel(pixelPosition, chars[i]));
+        }
+
+        return pixels.toArray(ConsolePixel[]::new);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+}
