b0730312a63974727d2077d94e5f36743837e49d
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-09-03 21:46:20 +1200

    Refactor engine config loader into a generic keyvalue file loader

--- src/main/java/net/scriptronix/snakegame/assets/EngineConfigLoader.java
+++ src/main/java/net/scriptronix/snakegame/assets/EngineConfigLoader.java
@@ -17,31 +17,9 @@ import net.scriptronix.snakegame.EngineConfig;
 public class EngineConfigLoader {
 
     public static EngineConfig load(String configPath) {
-        BufferedReader br = null;
-        HashMap<String, String> options = new HashMap<>();
+        HashMap<String, String> optionsMap = KeyValueFileLoader.loadFile(configPath);
 
-        try {
-            br = new BufferedReader(new FileReader(configPath));
-
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                StringTokenizer tokens = new StringTokenizer(line, "=");
-                if (tokens.countTokens() != 2)
-                    continue; // Invalid length. Try next line
-                options.put(tokens.nextToken(), tokens.nextToken());
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(EngineConfigLoader.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            if (br != null)
-                try {
-                br.close();
-            } catch (IOException ex) {
-                Logger.getLogger(EngineConfigLoader.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-
-        return EngineConfig.fromHashMap(options);
+        return EngineConfig.fromHashMap(optionsMap);
     }
 
     /**
@@ -53,26 +31,7 @@ public class EngineConfigLoader {
     public static void save(EngineConfig engineConfig, String savePath) {
         HashMap<String, String> ec = engineConfig.toHashMap();
 
-        StringBuilder saveFilebuffer = new StringBuilder();
-
-        for (Map.Entry<String, String> option : ec.entrySet()) {
-            String optionName = option.getKey();
-            String optionValue = option.getValue();
-            saveFilebuffer
-                    .append(optionName)
-                    .append("=")
-                    .append(optionValue)
-                    .append('\n');
-        }
-
-        FileOutputStream fileOut = null;
-        try {
-            fileOut = new FileOutputStream(savePath);
-            fileOut.write(saveFilebuffer.toString().getBytes());
-            fileOut.close();
-        } catch (Exception e) {
-            // Couldn't create a save file
-        }
+        KeyValueFileLoader.saveFile(ec, savePath);
     }
 
 }
---
+++ src/main/java/net/scriptronix/snakegame/assets/KeyValueFileLoader.java
@@ -0,0 +1,78 @@
+package net.scriptronix.snakegame.assets;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Loads files with Key=Value pairs into a HashMap<String, String>
+ *
+ * @author minee
+ */
+public class KeyValueFileLoader {
+
+    /**
+     * Loads a Key=Value file into a HashMap<String, String>
+     *
+     * @param filePath
+     * @return
+     */
+    public static HashMap<String, String> loadFile(String filePath) {
+        BufferedReader br = null;
+        HashMap<String, String> map = new HashMap<>();
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                StringTokenizer tokens = new StringTokenizer(line, "=");
+                if (tokens.countTokens() != 2)
+                    continue; // Invalid length. Try next line
+                map.put(tokens.nextToken(), tokens.nextToken());
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(KeyValueFileLoader.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        } finally {
+            if (br != null)
+                try {
+                br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(KeyValueFileLoader.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            }
+
+        }
+        return map;
+    }
+
+    public static void saveFile(HashMap<String, String> map, String savePath) {
+        StringBuilder saveFilebuffer = new StringBuilder();
+
+        for (Map.Entry<String, String> option : map.entrySet()) {
+            String optionName = option.getKey();
+            String optionValue = option.getValue();
+            saveFilebuffer
+                    .append(optionName)
+                    .append("=")
+                    .append(optionValue)
+                    .append('\n');
+        }
+
+        FileOutputStream fileOut = null;
+        try {
+            fileOut = new FileOutputStream(savePath);
+            fileOut.write(saveFilebuffer.toString().getBytes());
+            fileOut.close();
+        } catch (Exception ex) {
+            // Couldn't create a save file
+            Logger.getLogger(KeyValueFileLoader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
